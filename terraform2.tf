08b140582332c1ac440ce6fadf69dc7214bd0a87
Rushi <rushitest4559@gmail.com>
2025-08-28 12:51:59 +0530

    add terraform2.tf file

---
+++ terraform2.tf
@@ -0,0 +1,8 @@
+# demo file
+this is a demo file for testing
+demo content
+    more demo content
+    even more demo content
+    final demo content
+    additional demo content
+    even final demo content
